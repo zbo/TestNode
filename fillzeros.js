40d2a9b9f61addc435f970f1cd868b7de370f4e1
zhubo <zhubo@microstrategy.com>
2019-02-12 00:56:10 +0800

    first commit

---
+++ fillzeros.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const parse = require('csv-parse')
+const assert = require('assert')
+var filePath =__dirname+'/result.csv'
+
+
+fs.readFile(filePath, function (err, data) {
+    if (err) {
+        console.log(err.stack);
+        return;
+    }
+    parse(data.toString(),function(err, output){
+        process(output)
+    })
+});
+
+function process(data){
+    console.log(data.length)
+}
